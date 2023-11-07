0ca99334e50c8bbe5f13a6571dd5992d51b91e9e
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-11-07 18:12:35 +0545

    caching

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ Route::get('/', function () {
 });
 
 Route::get('/posts/{post}', function ($slug) {
-    $path= __DIR__  ."/../resources/posts/{$slug}.html";
-    if(!file_exists($path)){
+    if(!file_exists($path =__DIR__  ."/../resources/posts/{$slug}.html")){
         // dd("file doesn't exits.");
         // abort(404);
         return redirect('/');
     }
+    cache()->remember("posts.($slug)",5,fn()=>file_get_contents($path));
+
     $post = file_get_contents($path);
-    return view('post',[
-        'post'=>$post
-    ]);
-});
+    return view('post',['post'=>$post]);
+})->where('post', '[A-z_\-]+');
+
