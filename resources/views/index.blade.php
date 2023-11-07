13b3814484486064b97183b1f5a12a0ab8c771d7
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-11-07 15:36:30 +0545

    posting from file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/index.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Blogs</title>
+</head>
+
+<body>
+    <h1 class="blog"> Blogs Available</h1>
+    <article>
+        <h3>-First post</h3>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, rem eum. Laborum ullam voluptatem tempora
+            dolores. Quaerat voluptatibus suscipit consequuntur ad soluta vero reprehenderit perferendis rem. Illo harum
+            similique quasi?
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, qui. Neque, omnis temporibus! Itaque harum
+            magni libero suscipit explicabo, illum eveniet. Quidem atque quisquam autem fugit sapiente earum quis
+            consequuntur.</p>
+        <a href="/posts/first"><button type="submit">Read More...</button></a>
+    </article>
+    <article>
+        <h3>-Second post</h3>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, rem eum. Laborum ullam voluptatem tempora
+            dolores. Quaerat voluptatibus suscipit consequuntur ad soluta vero reprehenderit perferendis rem. Illo harum
+            similique quasi?
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, qui. Neque, omnis temporibus! Itaque harum
+            magni libero suscipit explicabo, illum eveniet. Quidem atque quisquam autem fugit sapiente earum quis
+            consequuntur.</p>
+            <a href="/posts/second"><button type="submit">Read More...</button></a>
+
+    </article>
+    <article>
+        <h3>-Third post</h3>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, rem eum. Laborum ullam voluptatem tempora
+            dolores. Quaerat voluptatibus suscipit consequuntur ad soluta vero reprehenderit perferendis rem. Illo harum
+            similique quasi?
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, qui. Neque, omnis temporibus! Itaque harum
+            magni libero suscipit explicabo, illum eveniet. Quidem atque quisquam autem fugit sapiente earum quis
+            consequuntur.</p>
+            <a href="/posts/third"><button type="submit">Read More...</button></a>
+        </article>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/posts/{post}', function ($slug) {
+    $path= __DIR__  ."/../resources/posts/{$slug}.html";
+    if(!file_exists($path)){
+        // dd("file doesn't exits.");
+        // abort(404);
+        return redirect('/');
+    }
+    $post = file_get_contents($path);
+    return view('post',[
+        'post'=>$post
+    ]);
 });
