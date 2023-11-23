6a7ce443c786f2133ef84e2983ab641e8b9f31fd
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-11-23 13:35:02 +0545

    feat: Update the layout of the posts pages

---
+++ resources/views/components/post-main-card.blade.php
@@ -0,0 +1,67 @@
+<article
+            class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+            <div class="py-6 px-5 lg:flex">
+                <div class="flex-1 lg:mr-8">
+                    <img src="./images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl" />
+                </div>
+
+                <div class="flex-1 flex flex-col justify-between">
+                    <header class="mt-8 lg:mt-0">
+                        <div class="space-x-2">
+                            <a href="#"
+                                class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold hover:bg-blue-300 hover:text-white"
+                                style="font-size: 10px">Techniques</a>
+
+                            <a href="#"
+                                class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold hover:bg-red-300 hover:text-white"
+                                style="font-size: 10px">Updates</a>
+                        </div>
+
+                        <div class="mt-4">
+                            <h1 class="text-3xl">
+                                This is a big title and it will look
+                                great on two or even three lines.
+                                Wooohoo!
+                            </h1>
+
+                            <span class="mt-2 block text-gray-400 text-xs">
+                                Published <time>1 day ago</time>
+                            </span>
+                        </div>
+                    </header>
+
+                    <div class="text-sm mt-2">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.
+                        </p>
+
+                        <p class="mt-4">
+                            Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat
+                            nulla pariatur.
+                        </p>
+                    </div>
+
+                    <footer class="flex justify-between items-center mt-8">
+                        <div class="flex items-center text-sm">
+                            <img src="./images/lary-avatar.svg" alt="Lary avatar" />
+                            <div class="ml-3">
+                                <h5 class="font-bold">Lary Laracore</h5>
+                                <h6>Mascot at Laracasts</h6>
+                            </div>
+                        </div>
+
+                        <div class="hidden lg:block">
+                            <a href="/post.html"
+                                class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
+                                More</a>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        </article>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,77 +1,153 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+    <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
+        <article
+            class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
+            <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
+                <img
+                    src="./images/illustration-1.png"
+                    alt=""
+                    class="rounded-xl" />
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Inter&family=Merriweather:ital,wght@1,700&family=Playpen+Sans:wght@300&family=Poppins:wght@200;300;500&display=swap');
+                <p class="mt-4 block text-gray-400 text-xs">
+                    Published <time>1 day ago</time>
+                </p>
 
-    body {
-        font-family: 'Playpen Sans', cursive;
-        background-color: #f0f0f0;
-        margin-top: 50px;
-    }
-
-    h1 {
-        font-weight: 700;
-        color: #333;
-        text-align: center;
-        font-size: 35px;
-        margin-bottom: 15px;
-    }
-
-    article {
-        margin-top: 50px;
-        margin: 0 auto;
-        width: 50%;
-        background-color: #fff;
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: 0 0 5px #ccc;
-    }
-
-    article+article {
-        margin-top: 20px;
-        margin-bottom: 20px;
-    }
-
-    a {
-        text-decoration: none;
-        color: #ff0000;
-    }
-
-    button {
-        cursor: pointer;
-        border: none;
-        background-color: #333;
-        color: #fff;
-        padding: 10px 20px;
-        border-radius: 5px;
-        font-size: 16px;
-        font-weight: 500;
-    }
-</style>
-
-<body>
-    <x-layout>
-        <article>
-            <h1>
-                {{ $post->title }}
-            </h1>
-            <p>By <a href="/authors/{{ $post->author->username }}">{{ $post->author->username }}</a> in
-                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>
-                {!! $post->body !!}
+                <div
+                    class="flex items-center lg:justify-center text-sm mt-4">
+                    <img
+                        src="./images/lary-avatar.svg"
+                        alt="Lary avatar" />
+                    <div class="ml-3 text-left">
+                        <h5 class="font-bold">Lary Laracore</h5>
+                        <h6>Mascot at Laracasts</h6>
+                    </div>
+                </div>
             </div>
-            <hr>
-            <a href="/"><button>Go Back</button></a>
-        </article>
-    </x-layout>
-</body>
 
-</html>
+            <div class="col-span-8">
+                <div class="hidden lg:flex justify-between mb-6">
+                    <a
+                        href="/index.html"
+                        class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-400">
+                        <svg
+                            width="22"
+                            height="22"
+                            viewBox="0 0 22 22"
+                            class="mr-2">
+                            <g
+                                fill="none"
+                                fill-rule="evenodd">
+                                <path
+                                    stroke="#000"
+                                    stroke-opacity=".012"
+                                    stroke-width=".5"
+                                    d="M21 1v20.16H.84V1z"></path>
+                                <path
+                                    class="fill-current"
+                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                            </g>
+                        </svg>
+
+                        Back to Posts
+                    </a>
+
+                    <div class="space-x-2">
+                        <a
+                            href="#"
+                            class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold hover:bg-blue-300 hover:text-white"
+                            style="font-size: 10px"
+                            >Techniques</a
+                        >
+                        <a
+                            href="#"
+                            class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold hover:bg-red-300 hover:text-white"
+                            style="font-size: 10px"
+                            >Updates</a
+                        >
+                    </div>
+                </div>
+
+                <h1 class="font-bold text-3xl lg:text-4xl mb-10">
+                    This is a big title and it will look great on two or
+                    even three lines. Wooohoo!
+                </h1>
+
+                <div class="space-y-4 lg:text-lg leading-loose">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.
+                    </p>
+
+                    <p>
+                        Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat
+                        nulla pariatur.
+                    </p>
+
+                    <p>
+                        Sed ut perspiciatis unde omnis iste natus error
+                        sit voluptatem accusantium doloremque
+                        laudantium, totam rem aperiam, eaque ipsa quae
+                        ab illo inventore veritatis et quasi architecto
+                        beatae vitae dicta sunt explicabo. Nemo enim
+                        ipsam voluptatem quia voluptas sit aspernatur
+                        aut odit aut fugit.
+                    </p>
+
+                    <h2 class="font-bold text-lg">
+                        Sed quia consequuntur
+                    </h2>
+
+                    <p>
+                        Magni dolores eos qui ratione voluptatem sequi
+                        nesciunt. Neque porro quisquam est, qui dolorem
+                        ipsum quia dolor sit amet, consectetur, adipisci
+                        velit, sed quia non numquam eius modi tempora
+                        incidunt ut labore et dolore magnam aliquam
+                        quaerat voluptatem.
+                    </p>
+
+                    <p>
+                        Ut enim ad minima veniam, quis nostrum
+                        exercitationem ullam corporis suscipit
+                        laboriosam, nisi ut aliquid ex ea commodi
+                        consequatur? Quis autem vel eum iure
+                        reprehenderit qui in ea voluptate velit esse
+                        quam nihil molestiae consequatur, vel illum qui
+                        dolorem eum fugiat quo voluptas nulla pariatur?"
+                    </p>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.
+                    </p>
+
+                    <p>
+                        Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat
+                        nulla pariatur.
+                    </p>
+
+                    <p>
+                        Sed ut perspiciatis unde omnis iste natus error
+                        sit voluptatem accusantium doloremque
+                        laudantium, totam rem aperiam, eaque ipsa quae
+                        ab illo inventore veritatis et quasi architecto
+                        beatae vitae dicta sunt explicabo. Nemo enim
+                        ipsam voluptatem quia voluptas sit aspernatur
+                        aut odit aut fugit.
+                    </p>
+                </div>
+            </div>
+        </article>
+    </main>
+
+</x-layout>
